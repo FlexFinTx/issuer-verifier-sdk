ab3c4dfa3207b25cb2eee76e4468d0f04b897d57
alph4x <danisharora099@gmail.com>
2020-08-10 22:02:44 +0530

    refactor: removed unused comments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-//DO NOT DELETE
-// I also want to learn how to type without looking at the keyboard  its so cool lol .aLSO YOU LOOK REALLY HOT WHEN YOU ARE CODING .(:))
 import FlexSDK from "./FlexSDK";
 const sdk = new FlexSDK("https://ivapi.orgx.flexfintx.com", "dummy-apikey");
 
